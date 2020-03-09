2d87c4cca6f07ecd0cb0eb215ee73044c34ea62c
Tan Nguyen <nguyendinhtan96@gmail.com>
2020-03-08 19:30:32 -0700

    Update Dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const personalPens = require('./PersonalPens.json');
+const projectPens = require('./ProjectPens.json');
+const collectionPens = require('./CollectionPens.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', {title: 'CodePen'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'CodePen Login'});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {title: 'CodePen Signup'});
+});
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', {title: 'Dashboard', personalPens, projectPens, collectionPens});
 });
 
 module.exports = router;
